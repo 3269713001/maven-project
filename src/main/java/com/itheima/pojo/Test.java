cc9d3c9592583b437e357f6ed66db277d6eecf8f
3269713001 <106867113+3269713001@users.noreply.github.com>
2022-06-04 19:16:24 +0800

    pull commit

--- src/main/java/com/itheima/pojo/Test.java
+++ src/main/java/com/itheima/pojo/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("master test");
         System.out.println("hot-fix test!");
         System.out.println("hot-fix2 test!");
+        System.out.println("pull test!");
     }
 }
